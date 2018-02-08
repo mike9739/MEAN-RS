0b362549e2017a5ccaf83556805c9196fa1cef01
mike9739 <migu_el_39@hotmail.com>
2018-02-07 21:05:07 -0600

    Se creo el control login y se agrego la función para detectar usuarios duplicados

--- curso-mean-social/api/controllers/user.js
+++ curso-mean-social/api/controllers/user.js
@@ -25,18 +25,37 @@ function saveUser(req,res){
 		user.email = params.email;
 		user.role = 'ROLE_USER'
 		user.image = null;
-		bcrypt.hash(params.password,null,null,(err,hash)=>{
+		
+		//Comprobación de usuarios duplicados
+		User.find({ $or: [
+							{email: user.email},
+							{nick: user.nick}
+							]}).exec((err,users) => {
+								if(err) return res.status(500).send({message:'Error en la petición de usuario'});
+
+								if (users && users.length >=1) {
+									return res.status(200).send({message:'el usuario que tratas de registrar ya existe '});
+								}
+								else{
+			//encriptación de las contraseñas y guarda los datos
+			bcrypt.hash(params.password,null,null,(err,hash)=>{
 			user.password = hash;
 			user.save((err,userStored)=>{
-				if(err) return res.status(500).send({message:'Error al guardar el usuario'});
-					if (userStored) {
+			if(err) return res.status(500).send({message:'Error al guardar el usuario'});
+			if (userStored) {
 						res.status(200).send({user : userStored});
-					}
-					else{
+							}
+			else{
 						res.status(404).send({message:'No se ha registrado el usuario'});
-					}
+				}
 			});
 		});
+
+								}
+
+
+							});
+	
 
 
 	}
@@ -49,8 +68,34 @@ function saveUser(req,res){
 
 }
 
+function loginUser(req,res){
+	var params =req.body;
+	var email = params.email;
+	var password = params.password;
+	User.findOne({email:email},(err,user) =>{
+		if(err) return res.status(500).send({message:'Error en la petición'});
+		if(user){
+			bcrypt.compare(password,user.password,(err,check) =>{
+				if(check){
+					//devolver datos de usuario
+					return res.status(200).send({user});
+
+
+				}
+				else{
+					return res.status(404).send({message:'El usuario no se ha podido identificar'});
+				}
+			});
+		}
+		else{
+			return res.status(404).send({message:'El usuario no se ha podido identificar'});
+		}
+	});
+}
+
 module.exports = {
 	home,
 	pruebas,
-	saveUser
+	saveUser,
+	loginUser
 }
