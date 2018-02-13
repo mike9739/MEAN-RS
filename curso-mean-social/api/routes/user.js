2eb6de3d79d1dd82d04367b31e6f563b33897403
mike9739 <migu_el_39@hotmail.com>
2018-02-12 22:21:30 -0600

    Se agrgo un middleware de autentificación de tokens

--- curso-mean-social/api/routes/user.js
+++ curso-mean-social/api/routes/user.js
@@ -4,10 +4,12 @@ var express = require('express');
 var UserController = require('../controllers/user');
 
 var api = express.Router();
+var md_auth = require('../middlewares/authentificated');
 
 api.get('/home', UserController.home);
-api.get('/pruebas',UserController.pruebas);
+api.get('/pruebas',md_auth.ensureAuth ,UserController.pruebas);
 api.post('/register',UserController.saveUser);
-api.post('/login',UserController.loginUser)
+api.post('/login',UserController.loginUser);
+
 
 module.exports = api;
