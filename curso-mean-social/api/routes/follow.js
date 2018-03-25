4a2f841897d68261c64e6c79a327d0301f29cbcf
mike9739 <migu_el_39@hotmail.com>
2018-03-25 13:13:13 -0600

    Se agrego la funcion de listar a los usuarios seguidos

--- curso-mean-social/api/routes/follow.js
+++ curso-mean-social/api/routes/follow.js
@@ -6,5 +6,6 @@ var md_auth = require('../middlewares/authentificated');
 
 api.post('/follow',md_auth.ensureAuth,FollowController.saveFollow);
 api.delete('/follow/:id',md_auth.ensureAuth,FollowController.deleteFollow);
+api.get('/following/:id?/:page?',md_auth.ensureAuth,FollowController.getFollowingUsers);
 
 module.exports = api;
