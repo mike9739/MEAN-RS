a75d437499617128033769445d1877831f73fd62
mike9739 <migu_el_39@hotmail.com>
2018-03-25 14:38:10 -0600

    se agreg la funcion para ver que usuarios te siguen

--- curso-mean-social/api/controllers/follow.js
+++ curso-mean-social/api/controllers/follow.js
@@ -66,10 +66,45 @@ function getFollowingUsers(req,res)
 	});
 
 }
+function getFollowedUser(req,res)
+{
+	var userId = req.user.sub;
+
+	if (req.params.id && req.params.page) {
+		userId = req.params.id;
+
+	}
+	var page = 1;
+
+	if (req.params.page) 
+	{
+		page = req.params.page;
+	}
+	else
+	{
+		page = req.params.id;
+	}
+	var itemsPerPage = 4;
+
+	Follow.find({followed:userId}).populate('user followed').paginate(page,itemsPerPage,(err,follows,total) => {
+		if(err) return res.status(500).send({message: 'Error en el servidor'});
+		if(!follows) return res.status(404).send({message:'No tienes seguidores :('});
+
+		return res.status(200).send({
+			total: total,
+			pages: Math.ceil(total/itemsPerPage),
+			follows
+
+		});
+
+
+	});
+
+}
 
 module.exports = {
 	saveFollow,
 	deleteFollow,
-	getFollowingUsers
-	
+	getFollowingUsers,
+	getFollowedUser
 }
--- curso-mean-social/api/routes/follow.js
+++ curso-mean-social/api/routes/follow.js
@@ -7,5 +7,6 @@ var md_auth = require('../middlewares/authentificated');
 api.post('/follow',md_auth.ensureAuth,FollowController.saveFollow);
 api.delete('/follow/:id',md_auth.ensureAuth,FollowController.deleteFollow);
 api.get('/following/:id?/:page?',md_auth.ensureAuth,FollowController.getFollowingUsers);
+api.get('/followed/:id?/:page?',md_auth.ensureAuth,FollowController.getFollowedUser);
 
 module.exports = api;
