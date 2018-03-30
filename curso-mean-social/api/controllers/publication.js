6ae9ca059bdcab8a80eb8fbc2297d0bfe920ff90
mike9739 <migu_el_39@hotmail.com>
2018-03-29 19:38:47 -0600

    Se agregaron las funciones : getPublication : devuelve  una publicacion con un id getPublications: devuelve las publicaciones de un usuario (preparativo para la timeline)

--- curso-mean-social/api/controllers/publication.js
+++ curso-mean-social/api/controllers/publication.js
@@ -12,11 +12,81 @@ var User = require('../models/user');
 //carga el modeko de follows
 var Follow = require('../models/follow');
 
+//  funcion para hacere preubas en el servidor
 function probando(req,res)
 {
 	res.status(200).send({message:'Primera publicación perros '});
 }
+// funcioon para guardar publicaciones
+function savePublication(req,res)
+{
+	var params = req.body;
+	var publication = new Publication();
+	if (!params.text) return res.status(200).send({message:'Debes agregar un texto'});
+
+	publication.text = params.text;
+	publication.file = 'null';
+	publication.user = req.user.sub;
+	publication.created_at = moment().unix();
+
+	publication.save((err,publicationStored)=>
+	{
+		if (err) return res.status(500).send({message:'Error al guardar la publicación'});
+		if (!publicationStored) return res.status(404).send({message:'La publicación no ha sido guardada'});
+
+		return res.status(200).send({publication: publicationStored});
+
+	});
+
+}
+
+//lista las publicaciones
+
+function getPublications(req,res)
+{
+	var page = 1;
+	if (req.params.page) 
+		{
+			page = req.params.page;
+		}
+
+	
+	var itemsPerPage = 4;
+	Follow.find({user: req.user.sub}).populate('followed').exec((err,follows)=>
+	{
+		if(err) return res.status(500).send({message: 'Error al devolver el seguimiento'});
+		var follows_clean = [];
+		follows.forEach((follow)=>{	
+			follows_clean.push(follow.followed);
+
+		});
+		
+		Publication.find({user: {"$in":follows_clean}}).sort('-created_at').populate('user').paginate(page,itemsPerPage,(err,publications,total)=>{
+			if(err) return res.status(500).send({message: 'Error al devolver publicación'});
+			if(!publications) return res.status(404).send({message: 'No hay publicaciones'});
+			return res.status(200).send({
+				total_items:total,
+				pages: Math.ceil(total/itemsPerPage),
+				page: page,
+				publications
+			});
+		});
+	});
+}
+function getPublication(req,res) {
+	var publicationId = req.params.id;
+
+	Publication.findById(publicationId,(err,publication) =>{
+		if(err) res.status(500).send({message: 'Error al devolver publicación'});
+		if(!publication) res.status(404).send({message: 'La publicación no existe'}); 
+		return res.status(200).send({publication});
+
+	});
+}
 
 module.exports ={
-	probando
+	probando,
+	savePublication,
+	getPublications,
+	getPublication
 }
--- curso-mean-social/api/routes/publication.js
+++ curso-mean-social/api/routes/publication.js
@@ -8,5 +8,8 @@ var multipart = require('connect-multiparty');
 var mp_upload = multipart({uploadDir: './uploads/publications'});
 
 api.get('/probando-pub',md_auth.ensureAuth,PublicationController.probando);
+api.post('/publication',md_auth.ensureAuth,PublicationController.savePublication);
+api.get('/publications/:page?',md_auth.ensureAuth,PublicationController.getPublications);
+api.get('/publication/:id',md_auth.ensureAuth,PublicationController.getPublication);
 
 module.exports = api;
