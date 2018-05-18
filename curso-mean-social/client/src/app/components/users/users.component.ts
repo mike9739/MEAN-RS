f6592da98c0ff2392b7f757a9ee31ee76939057d
mike9739 <migu_el_39@hotmail.com>
2018-05-17 20:32:09 -0500

    Se agregaron el control de gente

--- curso-mean-social/client/src/app/components/users/users.component.ts
+++ curso-mean-social/client/src/app/components/users/users.component.ts
@@ -15,6 +15,15 @@ export class UsersComponent implements OnInit {
 	public title:string;
 	public identity;
 	public token;
+	public page;
+	public next_page;
+	public prev_page;
+	public status: string;
+	public total;
+	public pages;
+	public users : User[];
+	public url;
+
 
 	constructor(
 		private _route: ActivatedRoute,
@@ -24,10 +33,61 @@ export class UsersComponent implements OnInit {
 		this.title = 'Gente';
 		this.identity = this._userService.getIdentity();
 		this.token = this._userService.getToken();
-
+		this.url = GLOBAL.url;
 	}
 	ngOnInit(){
-		console.log("user.component ha sido cargado")
+		console.log("user.component ha sido cargado");
+		this.actualPage();
+
+	}
+	actualPage()
+	{
+		this._route.params.subscribe(params => {
+			let page = +params['page'];
+			this.page = page;
+			if (!page) {
+				page =1;
+			}
+			else
+			{
+				this.next_page = page +1;
+				this.prev_page = page-1;
+
+				if (this.prev_page <=0) {
+					this.prev_page =1;
+				}
+			}
+
+			//devolver listado de usuarios
+			this.getUSers(page);
+
+		});
+	}
+	getUSers(page){
+		this._userService.getUsers(page).subscribe(response=>{
+			if (!response.users) {
+				this.status="error";
+
+			}
+			else
+			{
+			this.total = response.total;
+			this.users = response.users;
+			this.pages = response.pages;
+			if (page>this.pages) {
+				this._router.navigate(['/gente',1]);
+			}
+
+			}
+
+		},
+		error=>{
+			var errorMesage = <any>error;
+			console.log(errorMesage);
+			if (errorMesage != null) {
+				this.status = 'error'
+			}
+		});
 	}
 }
 
--- curso-mean-social/client/src/app/services/user.service.ts
+++ curso-mean-social/client/src/app/services/user.service.ts
@@ -93,5 +93,15 @@
 		return this._http.put(this.url+'update-user/'+user._id,params,{headers:headers});
 
 	}
+	getUsers(page = null):Observable<any>
+	{
+		let headers = new HttpHeaders().set('Content-Type','application/json').set('Authorization',this.getToken());
+		return this._http.get(this.url+'users/'+page,{headers:headers});
+	}
+	getUser(id):Observable<any>
+	{
+		let headers = new HttpHeaders().set('Content-Type','application/json').set('Authorization',this.getToken());
+		return this._http.get(this.url+'user/'+id,{headers:headers});
+	}
 
  }
